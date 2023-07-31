f551394c45363c80b0a184133ec4aa8a66b9b109
donghyunkim <ghffu405@gmail.com>
2023-07-31 17:27:09 +0900

    remove ignored file

--- src/main/java/july/lease/controller/ProductController_kdh.java
+++ src/main/java/july/lease/controller/ProductController_kdh.java
@@ -2,13 +2,17 @@ package july.lease.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-public class ProductController {
+public class ProductController_kdh {
 	
+	@GetMapping("/products/add")
+	public String add() {
+		
+		return "Project_product_upload";	
+	}
 
 }
