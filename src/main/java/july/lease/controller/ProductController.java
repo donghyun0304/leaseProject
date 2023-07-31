8a90b73232fe407b073676dc358774949f2957dc
donghyunkim <ghffu405@gmail.com>
2023-07-31 09:13:25 +0900

    test

---
+++ src/main/java/july/lease/controller/ProductController.java
@@ -0,0 +1,17 @@
+package july.lease.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Project_¿¹¾à";
+	}
+}
