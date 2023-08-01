9af54fe450ffb12850010bfc83a00ae79b90e18f
donghyunkim <ghffu405@gmail.com>
2023-08-01 15:36:07 +0900

    feat: Product클래스 categoryId2 필드 제거

--- src/main/java/july/lease/domain/Product.java
+++ src/main/java/july/lease/domain/Product.java
@@ -14,7 +14,6 @@ public class Product {
 	private Long productId;
 	private Long memberId;
 	private Long categoryId;
-	private Long categoryId2;
 	private String productName;
 	private Integer productPrice;
 	private String productContent;
@@ -26,11 +25,10 @@ public class Product {
 	public Product() {
 	}
 
-	public Product(Long memberId, Long categoryId, Long categoryId2, String productName, Integer productPrice, String productContent,
+	public Product(Long memberId, Long categoryId, String productName, Integer productPrice, String productContent,
 			String location) {
 		this.memberId = memberId;
 		this.categoryId = categoryId;
-		this.categoryId2 = categoryId2;
 		this.productName = productName;
 		this.productPrice = productPrice;
 		this.productContent = productContent;
