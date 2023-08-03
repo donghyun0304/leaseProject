23b437d8af4881de8ba13f63103f5f6eca63e06f
김승우 <tmd8633@naver.com>
2023-08-03 09:22:57 +0900

    최신화

---
+++ src/main/java/july/lease/MypageController_KSW/Dto/MyPageMainInfoDto.java
@@ -0,0 +1,21 @@
+package july.lease.MypageController_KSW.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@ToString
+@Setter
+@Getter
+public class MyPageMainInfoDto {
+
+	private String productId;
+	private String productImage;
+	private String productName;
+	private String startDate;
+	private String endDate;
+	private String countDate;
+	private String price;
+	
+}
