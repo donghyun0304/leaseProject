72776967ce328ecc3a44b88ef63a6d49795c2c80
김승우 <tmd8633@naver.com>
2023-08-16 18:37:15 +0900

    Add: 상세페이지 이미지 추가

--- src/main/webapp/resources/js/Project_product_details.js
+++ src/main/webapp/resources/js/Project_product_details.js
@@ -1,48 +1,59 @@
 window.addEventListener('load', function(){
 	
      // 이미지 배열 만들기
-     pics = ['tent1.jpg', 'tent2.jpeg', 'tent3.jpeg'
-     , 'tent4.jpeg', 'tent5.jpg'];
+	pics = [];
+     
+    $.ajax({
+    url: window.location.pathname + "/getImage",
+    type: 'GET',
+    success: function(responseData) {
+		pics = responseData;
+		console.log(pics);
+		// 컨테이너 박스의 배경 화면
+		// 배열의 0번째 인덱스값을 넣어 봅시다
+		// 1. 컨테이너박스 선택
+		// id로 접근
+		indexImg = 0;
+		
+		
+		// 컨테이너 박스의 백그라운드 설정
+		// 배열의 0번째 이미지를 보여줌
+		container.style.backgroundImage 
+		     = 'url(../../../../resources/images/'+ pics[indexImg] +')';
+		
+		// 왼쪽 버튼이 클릭 되었을때
+		// 0보다 작니
+		left.addEventListener('click', function(){
+		    indexImg--;
+		
+		if(indexImg < 0){
+		 // 마지막 그림으로 이동
+		 indexImg = pics.length - 1;
+		}
+		container.style.backgroundImage 
+		     = 'url(../../../../resources/images/'+ pics[indexImg] +')';
+		
+		});
+		
+		// 오른쪽 버튼이 클릭 되었을때
+		// 배열의 총길이보다 크거나 같니
+		right.addEventListener('click', function(){
+		    indexImg++;
+		
+		// 마지막인덱스인 경우 0으로 초기화
+		if(indexImg >= pics.length){
+		    indexImg = 0;
+		}
+		
+		container.style.backgroundImage 
+		     = 'url(../../../../resources/images/'+ pics[indexImg] +')';
+		});
+    },
+    error: function(error) {
+        console.error("Error:", error);
+    }
+    });
 
-	// 컨테이너 박스의 배경 화면
-	// 배열의 0번째 인덱스값을 넣어 봅시다
-	// 1. 컨테이너박스 선택
-	// id로 접근
-	indexImg = 0;
-	
-	
-	// 컨테이너 박스의 백그라운드 설정
-	// 배열의 0번째 이미지를 보여줌
-	container.style.backgroundImage 
-	     = 'url(image/'+ pics[indexImg] +')';
-	
-	// 왼쪽 버튼이 클릭 되었을때
-	// 0보다 작니
-	left.addEventListener('click', function(){
-	    indexImg--;
-	
-	if(indexImg < 0){
-	 // 마지막 그림으로 이동
-	 indexImg = pics.length - 1;
-	}
-	container.style.backgroundImage 
-	     = 'url(image/'+ pics[indexImg] +')';
-	
-	});
-	
-	// 오른쪽 버튼이 클릭 되었을때
-	// 배열의 총길이보다 크거나 같니
-	right.addEventListener('click', function(){
-	    indexImg++;
-	
-	// 마지막인덱스인 경우 0으로 초기화
-	if(indexImg >= pics.length){
-	    indexImg = 0;
-	}
-	
-	container.style.backgroundImage 
-	     = 'url(image/'+ pics[indexImg] +')';
-	});
 	
 	
 	
@@ -55,7 +66,7 @@ window.addEventListener('load', function(){
 	            if (parseInt(responseData) > 0) {
 	                alert('이 상품에 주문이 ' + responseData + '건 조회 되었습니다.\n삭제를 원하시면 마이페이지에서 대여취소 또는 대여완료를 해주세요.');
 	            } else if (parseInt(responseData) == 0) {
-	                if (confirm('삭제 하시겠습니까?')) {
+	                if (confirm('삭제하시겠습니까?')) {
 	                	
 	                	$.ajax({
 	                        url: window.location.pathname + "/delete",
